e81202ca7fe12630e97f280bf5c79cca7cfb2662
unknown <junsu0115@seoul.ac.kr>
2022-07-09 23:15:16 +0900

    log

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,11 +4,15 @@ from django.shortcuts import render, get_object_or_404
 from ..models import Question
 from django.db.models import Q, Count
 
+import logging
+logger = logging.getLogger('pybo')
+
 def index(request):
     """
     pybo 목록 출력
     """
-    3/0
+    logger.info("INFO 레벨로 출력")
+
     #입력 인자
     page=request.GET.get('page','1')
     kw = request.GET.get('kw', '')
